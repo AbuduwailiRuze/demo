e43e54deb35c823c8129a6fa66e4caad87d316c0
ali <407071905@qq.com>
2018-03-08 07:51:05 +0800

    3

--- src/main/java/com/example/demo/demo/App.java
+++ src/main/java/com/example/demo/demo/App.java
@@ -11,6 +11,7 @@ public class App
         System.out.println( "Hello World!" );
         System.out.println( "我在dev分支上修改的内容" );
         System.out.println( "我在dev修改了没提交" );
+        System.out.println( "我在dev修改的内容提交到本地" );
         
     }
 }
