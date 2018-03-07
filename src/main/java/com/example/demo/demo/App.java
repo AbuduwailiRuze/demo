21900765f0fcaab464c0ae1d8a14f45d1fe43e92
ali <407071905@qq.com>
2018-03-08 07:13:05 +0800

    11

--- src/main/java/com/example/demo/demo/App.java
+++ src/main/java/com/example/demo/demo/App.java
@@ -9,7 +9,5 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
-        System.out.println( "我在演示git 解决冲突" );
-        System.out.println( "远程版本修改了" );
     }
 }
